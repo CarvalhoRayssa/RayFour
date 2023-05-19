019a459ad7d91c57782900dcf7446647c8b1fb43
CarvalhoRayssa <112430158+CarvalhoRayssa@users.noreply.github.com>
2023-05-19 11:36:04 -0300

    Add files via upload

--- server2.js
+++ server2.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const pathToFile = __dirname + '/paises.json';
 
 http.createServer((req, res) => {
+
   if (req.url === '/paises') {
     // Define o cabeçalho da resposta para JSON
     res.setHeader('Content-Type', 'application/json');
@@ -22,11 +23,65 @@ http.createServer((req, res) => {
       // Analisa o conteúdo JSON
       const paises = JSON.parse(data);
 
+      var paises20primeiros = [];
+
+      paises.forEach((element, index) => { // Adiciona um parâmetro "index" à função forEach
+
+        if (index < 20) { // Verifica se o índice é menor que 20
+          paises20primeiros.push(element.nome_pais);
+        }
+
+      });
+
+      // Gera a resposta HTTP com o conteúdo JSON contendo apenas os primeiros 20 países
+      res.statusCode = 200;
+      res.end(JSON.stringify(paises20primeiros));
+    });
+  }
+
+  else if (req.url === '/paisescomb') {
+    // Define o cabeçalho da resposta para JSON
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Lê o arquivo paises.json
+    fs.readFile(pathToFile, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Erro interno do servidor');
+        return;
+      }
+
+      // Analisa o conteúdo JSON
+      const paises = JSON.parse(data);
+
+      var paisescomb = [];
+
+      paises.forEach(element => {
+
+        var nometemporario = element.nome_pais;
+
+        nometemporario = String(nometemporario);
+
+        var primeiraletra = nometemporario.substr(0,1);
+
+        if(primeiraletra == "B"){
+
+          paisescomb.push(element.nome_pais);
+
+          console.log(nometemporario);
+          
+        }
+        
+      });
+
       // Gera a resposta HTTP com o conteúdo JSON
       res.statusCode = 200;
-      res.end(JSON.stringify(paisesFiltrados));
+      res.end(JSON.stringify(paisescomb));
     });
-  } else {
+  }
+  else {
     // Se a URL não for /paises, envia o arquivo index.html
     res.writeHead(200, {
       'Content-Type': 'text/html',
@@ -35,6 +90,6 @@ http.createServer((req, res) => {
     let readStream = fs.createReadStream(__dirname + '/index.html');
     readStream.pipe(res);
   }
-}).listen(8000);
+}).listen(5000);
 
-console.log('Visite-me em: http://localhost:8000');
+console.log('Visite-me em: http://localhost:5000');
